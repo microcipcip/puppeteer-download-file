6be8e182cbfcad48c9b09272f09c9ad1bf76d494
Salvatore Tedde <salvatore.tedde@airtame.com>
2021-12-16 16:18:38 +0000

    Init

---
+++ index.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+// Step1: get a download folder
+const downloadPath = path.resolve('./download');
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch({
+      ignoreHTTPSErrors: true
+    });
+    const page = await browser.newPage();
+    await downloadFile(page)
+    await browser.close();
+  } catch (error) {
+    console.log(error)
+  }
+})();
+
+async function downloadFile(page) {
+  // Step2: set where I want to download the file
+  await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath });
+
+  await page.goto('https://10.21.0.2:5100/');
+  await clickButtons(page)
+
+  // Step3: wait for networkIdle so that we are sure the download is completed
+  await page.waitForNetworkIdle({ waitUntil: 'networkidle0' })
+}
+
+async function clickButtons (page) {
+  await clickBtn('Continue without audio/or video');
+  await page.click('[icon="bug-report"');
+  await clickBtn('Download report');
+
+  async function clickBtn (txt) {
+      const btn = await page.$x(`//paper-button[contains(text(), \'${txt}\')]`)
+      await btn[0].click()
+  }
+}
